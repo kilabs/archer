9f49f635ce58c7d80912615e18498e3b736955db
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-09-01 11:30:56 +0700

    premium takeout

--- protected/views/front/site/index.php
+++ protected/views/front/site/index.php
@@ -64,7 +64,6 @@
           <div class="verified-badge">
             <i class="icon icon-check"></i>
           </div>
-          <div class="premium-ribbon t_u">Premium</div>
           <a class="listing-image block" href="<?php echo Yii::app()->createUrl('post/detail',array('id'=>$value->id,'slug'=>$value->slug)); ?>">
             <img alt="" class="block" src="<?php echo LUpload::thumbs('Post',$value->foto,'615x430'); ?>">
           </a>
