52a99d85d0963d1997ed683aa81cd6c4d99de140
Arie Prasetyo <prasetyo.mailbox@gmail.com>
2014-09-09 08:53:26 +0700

    update locale reset password

--- protected/views/front/site/activeSend.php
+++ protected/views/front/site/activeSend.php
@@ -1,4 +1,4 @@
 <div class="alert alert-success" role="alert">
-	Link for activation user has been send to your email. please check and follow the instruction <br>
-	klik <?php echo CHtml::link('here',array('site/login')) ?> to login page , klik <?php echo CHtml::link('here',array('site/sendEmailKonfirmation')); ?> to resend email
+	<p>Link untuk aktifasi keanggotaan Anda sudah dikirim ke email Anda, silahkan cek dan ikuti instruksi yang menyertainya.</p>
+	<p>Klik <?php echo CHtml::link('disini',array('site/login')) ?> untuk login , klik <?php echo CHtml::link('disini',array('site/sendEmailKonfirmation')); ?> Kirim Ulang verifikasi.</p>
 </div>
