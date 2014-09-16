4eef78a54c121cc4969dae2ffaa090ba5b04414a
lateph <lateph@gmail.com>
2014-09-16 14:05:06 +0700

    bug fix slow import

--- protected/migrations/m140916_055526_lokasi.php
+++ protected/migrations/m140916_055526_lokasi.php
@@ -10,15 +10,22 @@ class m140916_055526_lokasi extends CDbMigration
 		$file = YiiBase::getPathOfAlias('application.data').'/lokasi.txt';
 		$content = file_get_contents($file);
 		$rows = explode("\n", $content);
+		$list = array();
 		for ($i=1; $i < count($rows); $i++) { 
 			$row = explode('|', $rows[$i]);
-			$this->insert('lokasi',array(
+			$list[] = array(
 				'nama'=>$row[0],
 				'lat'=>$row[3],
 				'lng'=>$row[2],
 				'negara'=>$row[5],
-			));
+			);
 		}
+
+		$insert = array_chunk($list, 1000);
+		foreach ($insert as $key => $value) {
+			$this->getDbConnection()->getCommandBuilder()->createMultipleInsertCommand('lokasi',$value)->execute();
+		}
+		//
 	}
 
 	public function down()
